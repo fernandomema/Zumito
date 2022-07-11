7a5f523e755104a0986aaecef3cf46caf5254557
WilliamAcosta1 <williamacosta00814@gmail.com>
2022-07-11 09:16:48 -0500

    ✨Agregar a slashcommands y bugs arreglados

--- commands/info/github.js
+++ commands/info/github.js
@@ -7,22 +7,25 @@ require("@modules/localization.js");
 
 module.exports = {
     name: "github",
-    description: "shows github statistics of a user",
     aliases: [],
-    category: "Info",
-    ussage: null,
-    hidden: false,
+    examples: [],
+    permissions: [],
+    botPermissions: [],
+    hidden: true,
     admin: false,
     nsfw: false,
+    cooldown: 0,
+    slashCommand: true,
+    dm: true,
 
     async execute(client, message, args) {
 
         if (!args.has(0)) {
-            return await message.reply({ 
-                content: emojis.deny + 'command.github.valid_username'.trans(), 
-                allowedMentions: { 
-                    repliedUser: false 
-                } 
+            return await message.reply({
+                content: emojis.deny + 'command.github.valid_username'.trans(),
+                allowedMentions: {
+                    repliedUser: false
+                }
             });
         }
 
@@ -31,34 +34,23 @@ module.exports = {
             .catch(async err => { });
 
         if (git === undefined || git.name === undefined) {
-            return await message.reply({ 
-                content: emojis.deny + 'command.github.enter_valid_user'.trans(), 
-                allowedMentions: { 
-                    repliedUser: false 
-                } 
+            return await message.reply({
+                content: emojis.deny + 'command.github.enter_valid_user'.trans(),
+                allowedMentions: {
+                    repliedUser: false
+                }
             });
         }
 
-        let embed = new Discord.MessageEmbed() 
-            .setAuthor({
-                name: 'command.github.author'.trans()+ ' ' + git.name,
-                url: git.html_url,
-                iconURL: git.avatar_url,
-            })
+        let embed = new Discord.MessageEmbed()
+            .setAuthor({ name: 'command.github.author'.trans() + ' ' + git.name,url: git.html_url, iconURL: git.avatar_url, })
             .setDescription('**' + 'command.github.bio'.trans() + '**' + `\n${git.bio || 'command.github.not_provided'.trans()}`) // Not Provided
             .addField('command.github.user_info'.trans(), "**" + 'command.github.real_name'.trans() + "**: " + `${git.login || 'command.github.not_provided'.trans()}` + "\n" + " **" + 'command.github.location'.trans() + "**: " + `${git.location || 'command.github.not_provided'.trans()}` + "\n" + "**" + 'command.github.github_id'.trans() + "**: " + `${git.id}`, true)
             .addField('command.github.social_stats'.trans(), "**" + 'command.github.followers'.trans() + "**: " + `${git.followers}` + "\n" + "**" + 'command.github.following'.trans() + "**: " + `${git.following}`, true)
             .setImage(git.avatar_url)
             .setColor(botConfig.embeds.color)
-            .setFooter({ text: getFooter(message.author.username), iconURL: message.author.avatarURL({ dynamic: true }) })
-            .setTimestamp();
+            .setFooter({ text: getFooter((message.author || message.member.user).tag), iconURL: (message.author || message.user).avatarURL({ dynamic: true }) })
 
-        await message.reply({ 
-            embeds: [embed], 
-            allowedMentions: { 
-                repliedUser: false 
-            } 
-        });
-
+        await message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
     }
 }
