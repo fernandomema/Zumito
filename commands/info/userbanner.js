7ef3c547059f08986111b4b0edd97a844d188bea
WilliamAcosta1 <williamacosta00814@gmail.com>
2022-07-11 20:29:17 -0500

    💖New command to make it functional and add it to the slashcommands.

--- commands/info/userbanner.js
+++ commands/info/userbanner.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const { getFooter } = require("@modules/utils/data.js");
 const botConfig = require('@config/bot.js');
+const emojis = require('@config/emojis.js'); 
 require("@modules/localization.js");
 
 module.exports = {
@@ -14,16 +15,19 @@ module.exports = {
     dm: true,
 
     async execute(client, message, args) {
-
-        const embed = new Discord.MessageEmbed()
-
-            .setTitle("command.userbanner.title".trans() + " " + "WilliamAcosta#0001" ) //Banner of
-            .setDescription("**" + "command.userbanner.fullImage".trans() + "**\n" + "[" + "command.userbanner.clickHere".trans() + "]" + "(" + `https://images-ext-1.discordapp.net/external/AbVtVcRDPHaeR6YzRxvACvl0cmPhrb_Vl1P8-V-tnpg/%3Fsize%3D2048/https/cdn.discordapp.com/banners/963953391061585972/a_043a2a4155868430140eb6d647e94d92.gif`+ ")")
-            .setImage("https://images-ext-1.discordapp.net/external/AbVtVcRDPHaeR6YzRxvACvl0cmPhrb_Vl1P8-V-tnpg/%3Fsize%3D2048/https/cdn.discordapp.com/banners/963953391061585972/a_043a2a4155868430140eb6d647e94d92.gif")
-            .setColor(botConfig.embeds.color)
-            .setFooter({ text: getFooter((message.author || message.member.user).tag), iconURL: (message.author || message.user).avatarURL({ dynamic: true }) })
-
-        return message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
+        
+        //Mensaje de usaurio sin banner.
+        return message.reply({ content: emojis.deny + " " +"**" + "WilliamAcosta#0001" + "**" + " " + "command.userbanner.noBanner".trans() , allowedMentions: { repliedUser: false } });
 
     }
 }
+
+//Mensaje donde va el banner del usuario.
+
+/* const embed = new Discord.MessageEmbed()
+
+            .setTitle("command.userbanner.title".trans() + " " + "WilliamAcosta#0001" )
+            .setDescription("**" + "command.userbanner.fullImage".trans() + "**\n" + "[" + "command.userbanner.clickHere".trans() + "]" + "(" + `https://images-ext-1.discordapp.net/external/AbVtVcRDPHaeR6YzRxvACvl0cmPhrb_Vl1P8-V-tnpg/%3Fsize%3D2048/https/cdn.discordapp.com/banners/963953391061585972/a_043a2a4155868430140eb6d647e94d92.gif`+ ")")
+            .setImage("https://images-ext-1.discordapp.net/external/AbVtVcRDPHaeR6YzRxvACvl0cmPhrb_Vl1P8-V-tnpg/%3Fsize%3D2048/https/cdn.discordapp.com/banners/963953391061585972/a_043a2a4155868430140eb6d647e94d92.gif")
+            .setColor(botConfig.embeds.color)
+            .setFooter({ text: getFooter((message.author || message.member.user).tag), iconURL: (message.author || message.user).avatarURL({ dynamic: true }) })*/
